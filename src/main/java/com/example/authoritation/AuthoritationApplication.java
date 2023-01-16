c9aed80cc6ecc524b1a84afe15c4200a956cec5c
ralapont <rafael.alapont.gomez@gmail.com>
2023-01-16 21:28:47 +0100

    Initial commit

---
+++ src/main/java/com/example/authoritation/AuthoritationApplication.java
@@ -0,0 +1,13 @@
+package com.example.authoritation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthoritationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthoritationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authoritation/AuthoritationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authoritation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthoritationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
